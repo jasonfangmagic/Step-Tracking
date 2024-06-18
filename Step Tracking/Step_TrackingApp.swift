e0f4af13259914262dda2e10c3c7211e6c52ecf6
Jason Fang <jasonfang623@gmail.com>
2024-06-18 16:16:43 -0400

    Initial Commit

---
+++ Step Tracking/Step_TrackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Step_TrackingApp.swift
+//  Step Tracking
+//
+//  Created by Jason Fang on 6/18/24.
+//
+
+import SwiftUI
+
+@main
+struct Step_TrackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
